317adab7374e918db5999a298cd017f0f879742f
dkhai02 <duckhai2002vn@gmail.com>
2024-07-11 15:52:07 +0700

    init project

---
+++ TokenAPI/Controllers/StreamTokenController.cs
@@ -0,0 +1,42 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TokenAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StreamTokenController : Controller
+    {
+        private readonly string _apiKey = "9puabwjb5p2p";
+        private readonly string _apiSecret = "xs93ftx87nujrm35z6eat7uzge5jky74hcby9umye2g4sdcdurkc25hmj8jf3gak";
+
+        [HttpGet]
+        [Route("generate")]
+        public IActionResult GenerateToken(string userId)
+        {
+            if (string.IsNullOrEmpty(userId))
+            {
+                return BadRequest("userId is required");
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_apiSecret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("user_id", userId),
+                    new Claim("role", "user")
+                }),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+            return Ok(new { token = tokenString });
+        }
+    }
+}
